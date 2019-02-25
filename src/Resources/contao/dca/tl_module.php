fad5f3a5dc31ab0c3795af5f6dfa1a186376e182
Alexander Naumov <me@alexandernaumov.de>
2019-02-25 17:01:58 +0100

    add nc select field

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -2,6 +2,19 @@
 
 $GLOBALS['TL_DCA']['tl_module']['palettes']['personalDataExtension'] = '{title_legend},name,headline,type;{config_legend},editable;{redirect_legend},jumpTo;{template_legend:hide},memberExtendedTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID';
 
+if ( class_exists( 'NotificationCenter\tl_nc_notification' ) ) {
+
+    if ( strpos( $GLOBALS['TL_DCA']['tl_module']['palettes']['personalDataExtension'], 'newsletters' ) ) {
+
+        $GLOBALS['TL_DCA']['tl_module']['palettes']['personalDataExtension'] = str_replace( 'newsletters;', 'newsletters,nc_notification;', $GLOBALS['TL_DCA']['tl_module']['palettes']['personalDataExtension'] );
+    }
+
+    else {
+
+        $GLOBALS['TL_DCA']['tl_module']['palettes']['personalDataExtension'] = str_replace( 'editable;', 'editable,nc_notification;', $GLOBALS['TL_DCA']['tl_module']['palettes']['personalDataExtension'] );
+    }
+}
+
 $GLOBALS['TL_DCA']['tl_module']['fields']['memberExtendedTpl'] = [
 
     'label' => &$GLOBALS['TL_LANG']['tl_module']['memberTpl'],
